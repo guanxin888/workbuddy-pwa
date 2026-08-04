5bace87aec68492253b4ecd75c7a388721cbb6eb
guanxin888 <550892736@qq.com>
2026-08-04 17:17:31 +0800

    add pwa config

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'workbuddy-cache-v1';
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
